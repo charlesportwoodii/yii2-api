830ebb28f64f374da1e924d1c8f49fe9da5adfd1
Charles R. Portwood II <charlesportwoodii@erianna.com>
2016-11-08 17:09:29 -0600

    Updating refresh cest test order

--- tests/api/v1/user/RefreshCest.php
+++ tests/api/v1/user/RefreshCest.php
@@ -39,21 +39,12 @@ class RefreshCest extends AbstractApiCest
 
         $newTokens = \json_decode($I->grabResponse(), true)['data'];
         
-        // Verify the tokens are wiped and that existing tokens cannot be reused
-        $I->wantTo('verify refresh token is wiped');
-        $payload = [
-            'refresh_token' => $I->getTokens()['refresh_token']
-        ];
-        $I->sendAuthenticatedRequest($this->uri, 'POST', $payload);
-        $I->seeResponseIsJson();
-        $I->seeResponseCodeIs(401);
-
         // Verify that the new tokens do work on an authenticated request
         $I->wantTo('verify new refresh tokens work');
         $payload = [
             'refresh_token' => $newTokens['refresh_token']
         ];
-
+        
         $I->addTokens($newTokens);
         $I->sendAuthenticatedRequest($this->uri, 'POST', $payload);
 
@@ -72,9 +63,19 @@ class RefreshCest extends AbstractApiCest
             ],
             'status' => 'integer'
         ]);
+
+        $newTokens = \json_decode($I->grabResponse(), true)['data'];
+        
+        // Verify the tokens are wiped and that existing tokens cannot be reused
+        $I->wantTo('verify refresh token is wiped');
+        $payload = [
+            'refresh_token' => $I->getTokens()['refresh_token']
+        ];
+        $I->sendAuthenticatedRequest($this->uri, 'POST', $payload);
+        $I->seeResponseIsJson();
+        $I->seeResponseCodeIs(401);
     }
 
-    
     public function testRefreshWithInvalidToken(\ApiTester $I)
     {
         $I->register(true);
