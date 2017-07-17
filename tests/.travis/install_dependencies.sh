6f62e5bd1c34591148350eda1353581149a7b45c
Charles R. Portwood II <charlesportwoodii@erianna.com>
2017-07-17 14:39:53 -0500

    sudo make intall libsodium

--- tests/.travis/install_dependencies.sh
+++ tests/.travis/install_dependencies.sh
@@ -7,5 +7,5 @@ cd $HOME;
 git clone -b $LIBSODIUMVERSION https://github.com/jedisct1/libsodium.git libsodium-$LIBSODIUMVERSION;
 cd $HOME/libsodium-$LIBSODIUMVERSION;
 ./autogen.sh;
-./configure
-sudo CFLAGS="-fPIC" make install;
+./configure;
+sudo make install;
